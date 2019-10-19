498d37732d142a0e40339624854ec0ae65b455c4
Gmangray4 <31903322+Gmangray4@users.noreply.github.com>
2019-10-19 10:58:28 -0400

    dfg

---
+++ 351/exercises/5/index.php
@@ -0,0 +1,147 @@
+<?php
+
+if($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+  //logs the values in array (is to help ... )
+  // var_dump($_POST);
+  /* Step 1:
+  Get the temperature value (a_temp): is a string so we need to convert to a number using the in built function intval()
+  */
+
+$temp = $_POST['a_temp'];
+
+$tempIntV = intval($temp);
+// echo ("<br />");
+// echo ($tempIntV);
+// echo ("<br />");
+
+
+  /* Step 2:
+  Get the radio button choice (tempChoice) and then create a variable to hold the converted value::
+  PLEASE USE THE FORMULA BELOW TO WRITE THE CONVERSION ALGORITHM - DO NOT USE ANY LIBRARY ETC
+  To convert from Fahrenheit to Celsius: Celsius = (5 / 9) * (Fahrenheit – 32)
+  To convert from Celsius to Fahrenheit: Fahrenheit = (9 / 5) * Celsius + 32
+*/
+
+$tempstate = $_POST['tempChoice'];
+
+if ($tempstate == "Fahrenheit") {
+  // code..
+
+$total = (9/5) * ($tempIntV + 32);
+// Fahrenheit = (9 / 5) * Celsius + 32
+}else {
+
+$total = (5 / 9) * ($tempIntV - 32);
+
+}
+
+// echo ($total);
+  /* Step 3:
+  Get the email address and name from the $_POST array (using the appropriate keys) and send an email
+  to that person containing the converted value. Please include a short message in the email (not JUST the value.)
+  Use the php inbuilt mail() function
+  https://www.w3schools.com/php/func_mail_mail.asp
+  The email may end up in your spam folder when testing...
+  */
+
+$email = $_POST['a_email'];
+$name = $_POST['a_name'];
+// $msg = ("<p class=msg>")."Your convented vaule is ".$total." in ".$tempstate.".".("</p>");
+// $msg2 = ("<p class=msg2>")."We are sorry ".$name.".".("</p>");
+// $msg3 = ("<p class=msg3>")."Our system could not send anything to ".$email.("</p>");
+// $msg4 = ("<p class=msg4>")."Stay Hydrated!".("</p>");
+
+// mail($email,$name,$msg);
+
+
+  /* Step 4: using the echo() - display a custom message i.e. Dear ... to notify the usewr that t
+  hey will get an email with the results.*/
+// echo ($msg);
+
+$theFile = fopen("files/testFile.txt", "w") or die("Unable to open");
+fwrite($theFile,$tempstate."\n");
+fwrite($theFile,$total."\n");
+fwrite($theFile,$name."\n");
+fwrite($theFile,$email."\n");
+
+
+header("Location: read.php");
+exit;
+
+
+}
+ ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>Sample Contact and FahrenHeit/Celsius Form </title>
+<!--set some style properties::: -->
+<script src="jquery/jquery-3.4.1.js"></script>
+<link rel="stylesheet" type="text/css" href="css/galleryStyle.css">
+</head>
+<body>
+
+<div class= "formContainer">
+<!--form -->
+<!-- You need an action att and a method att within the form tag -->
+<form action="index.php" method="post">
+<!-- group the related elements in a form -->
+<h3> LET US SEND YOU A CONVERTED VALUE F->C or C->F</h3>
+<fieldset>
+ <p><label>Name:</label><input type="text" size="40" maxlength = "40" name = "a_name" required> </p>
+<p><label>Email:</label> <input type ="email" size="40" maxlength = "40" name = 'a_email' required/></p>
+
+</fieldset>
+<fieldset>
+  <!-- ONLY number entries  -->
+  <p><label>Temperature Input:</label><input type="number" size="5" maxlength = "5" name = "a_temp" required> </p>
+  <p><input class="ConvertButt"type="radio" name="tempChoice" value="Celsius"> Convert to Celsius </p>
+  <p><input class="ConvertButt"type="radio" name="tempChoice" value="Fahrenheit"> Convert to Fahrenheit</p>
+  </fieldset>
+<!-- submit button  -->
+ <p><input type = "submit" name = "submit" value = "submit and convert" id =buttonS /></p>
+
+<p class=""></p>
+
+</form>
+</div>
+<script>
+
+$(document).ready (function(){
+    $("#insertTest").submit(function(event) {
+       //stop submit the form, we will post it manually. PREVENT THE DEFAULT behaviour ...
+      event.preventDefault();
+      console.log("button clicked");
+      let form = $('#insertTest')[0];
+      let data = new FormData(form);
+      for (let valuePairs of data.entries()) {
+       console.log(valuePairs[0]+ ', ' + valuePairs[1]);
+     }
+     $.ajax({
+               type: "POST",
+               enctype: 'text/plain',
+               url: "exerciseForm.php",
+               data: data,
+               processData: false,//prevents from converting into a query string
+               contentType: false,
+               cache: false,
+               timeout: 600000,
+               success: function (response) {
+               //reponse is a STRING (not a JavaScript object -> so we need to convert)
+               console.log("we had success!");
+               console.log(response);
+              },
+              error:function(){
+             console.log("error occurred");
+           }
+         });
+  });
+   });
+
+</script>
+
+</body>
+</html>
---
+++ 351/exercises/5/read.php
@@ -0,0 +1,231 @@
+<?php
+//check if there has been something posted to the server to be processed
+if($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET["ajaxTest"]))
+{
+ $theFile = fopen("files/testFile.txt", "r") or die("Unable to open file!");
+
+// have  every 3 lines an object ... ]
+$counter =0;
+$numberAttsPerObject =4;
+$outArr = array();
+   while(!feof($theFile)) {
+
+   // for every 3rd line  - create a NEW PHP object
+     if($counter%$numberAttsPerObject ==0){
+       $pData=new stdClass();
+     }
+     // keep track of the keys ..
+    //for each attribute in our php object we want a key  - 0,1 or 2
+     $i = $counter%$numberAttsPerObject;
+     // assign ... AND TRIM (remove white space)
+     $pData->$i = trim(fgets($theFile));
+
+     // if we have JUST completed the 3rd attribute and we are not at 0 -> write the object to the array
+     if($i ==$numberAttsPerObject-1 && $counter!=0){
+       array_push($outArr, $pData);
+     }
+     // increment the counter ...
+     $counter = $counter+1;
+
+   }
+
+   fclose($theFile);
+  // var_dump($outArr);
+  // Now we want to JSON encode these values to send them to $.ajax success.
+  $myJSONObj = json_encode($outArr);
+  echo $myJSONObj;
+  exit;
+ }
+// exit;
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Same Form Ex BUT READ </title>
+<!-- get JQUERY -->
+  <script src = "jquery/jquery-3.4.1.js"></script>
+<link rel="stylesheet" type="text/css" href="css/galleryStyle2.css">
+</head>
+<body>
+  <!-- NEW for the result -->
+<div id = "result"></div>
+<canvas id="canvas" width="300" height="300"></canvas>
+<div id = "result2"></div>
+
+<script>
+// here we put our JQUERY
+
+
+
+$(document).ready (function(){
+console.log("in doc load");
+
+let tempSpeed;
+
+$.ajax({
+  url: "read.php",
+  type: "get", //send it through get method
+  data: {
+    ajaxTest: "fread"
+  },
+  success: function(response) {
+    //Do Something
+    // console.log("responded" +response);
+    //use the JSON .parse function to convert the JSON string into a Javascript object
+    let parsedJSON = JSON.parse(response);
+  //  console.log(parsedJSON);
+    displayResults(parsedJSON);
+  },
+  error: function() {
+    console.log("error occurred");
+  }
+});
+
+// dd?
+/* STEP 1:
+/* THis is showing you how to access the canvas associated with the first box
+It also shows you how to access the associated drawing context
+and adding the relevent event listeners. You can follow this code for
+accessing the other canvases and their associated contexts.
+*/
+
+let canvas = document.getElementById("canvas")
+//get the context
+let context = canvas.getContext("2d");
+
+
+// register event listeners with 1st box
+// canvas.addEventListener('mousemove',mouseoverhandler );
+
+
+/*** The lists of objects that will be indide each canvas **/
+let rectList = []; // variable to hold your list of rectangles
+
+/* STEP 2:: CREATE 10 RectShapeObject instances and put into the rectList */
+const NUM_RECTS=10;
+for (let i=0; i <  NUM_RECTS;i++){
+  rectList.push(new RectShapeObject(i,i,50,50,context,canvas,(i%5)+1,(i%7)+1));
+};
+
+/**** ANIMATION CODE *****************/
+requestAnimationFrame(animationLoop);
+ /*MAIN ANIMATION LOOP */
+function animationLoop(){
+
+
+    // put code here to display and update contents in canvasAniA
+    /* STEP 6:: go through the rectList  and display and update shapes  */
+
+  context.clearRect(0,0,canvas.width,canvas.height);
+  for (let i = 0; i < rectList.length; i++) {
+    rectList[i].display();
+    rectList[i].update();
+  }
+
+  }
+  requestAnimationFrame(animationLoop);
+
+
+/***** OBJECT DEFINITIONS  ***********************/
+/* OBJECT DEFINITION FOR A SQUARE OBJECT SHAPE
+constructor takes an initial xpos, ypos, width and height for the shape.
+You also need to give the context and canvas associated
+with the potential instance of this shape
+*/
+
+
+function RectShapeObject(x,y,w,h,context,canvas, speedX,speedY){
+  this.context =context;
+  this.canvas = canvas;
+  this.x =x;
+  this.y =y;
+  this.w=w;
+  this.h=h;
+
+  this.innerW = this.w/2;
+  this.innerH = this.h/2;
+
+  this.innerX = this.x + this.innerW/2;
+  this.innerY = this.y + this.innerH/2;
+
+  this.speedX = speedX;
+  this.speedY= speedY;
+
+
+  // method to display - needs to be filled in
+  this.display = function(){
+    this.context.fillStyle ="white";
+    // context.fillStyle = "#ff0000"
+  this.context.fillRect(this.x, this.y, this.w, this.h);
+  this.context.clearRect(this.innerX, this.innerY, this.innerW, this.innerH);
+  }
+
+  // method to update (animation) - needs to be filled in
+  this.update = function(){
+    if (this.x +this.w>this.canvas.width || this.x <0) {
+    this.speedX*=-1;
+    }
+
+    if ((this.y+this.h)>this.canvas.height || this.y <0) {
+    this.speedY*=-1;
+    }
+
+      this.x+=this.speedX;
+      this.y+=this.speedY;
+      this.innerX = this.x + this.innerW/2;
+      this.innerY = this.y + this.innerH/2;
+    }
+}
+
+
+
+
+// dddd
+
+function displayResults(parsedJSON){
+  for(let i =0; i < parsedJSON.length; i++){
+     console.log(parsedJSON[i]);
+    let cObj  = parsedJSON[i];
+    // let title = $('<h3>').text("OBJECT READ::"+i);
+    // title.appendTo("#result");
+    // for (let property in cObj) {
+
+
+
+      let iNum = parseInt(cObj[1]); //Output will be 23.
+      console.log(iNum);
+let para2;
+      if(iNum < 0){
+
+        $("canvas").css("background-color", "blue");
+        tempSpeed = 500;
+        para2 = $('<p>').text("The cold air makes us slow!");
+      }
+
+      if(iNum > 0){
+
+        $("canvas").css("background-color", "red");
+        tempSpeed = 10;
+        para2 = $('<p>').text("The warm air makes us fast!");
+      }
+
+setInterval(function(){animationLoop()},tempSpeed);
+
+
+let para = $('<p>').text("Your converted result is "+ cObj["1"]+ " in "+cObj["0"]);
+para.appendTo("#result");
+para2.appendTo("#result2");
+    // }
+  }
+}
+
+
+
+
+
+
+}); //on load
+</script>
+</body>
+</html>
